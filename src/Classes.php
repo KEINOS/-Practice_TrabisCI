766f567a1d84eb6f32485304d7922feaab5e9f75
KEINOS <github@keinos.com>
2018-04-20 21:26:59 +0900

    fix: メソッドのスタティックコール対応

--- src/Classes.php
+++ src/Classes.php
@@ -2,8 +2,13 @@
 
 class MyClass
 {
-    public function return_message($message)
+    public static function return_message($message)
     {
         return (string) $message;
+    }
+
+    public function echo_message($message)
+    {
+        echo $this->return_message($message);
     }
 }
