2c8d9c8c7e1706074e513fdfb50a10b43180df6b
KEINOS <github@keinos.com>
2018-04-20 20:27:59 +0900

    chore: Class メソッド用

---
+++ tests/ClassessTest.php
@@ -0,0 +1,14 @@
+<?php
+include('vendor/autoload.php');
+include('src/Functions.php');
+include('src/ClassessTest.php');
+
+use PHPUnit\Framework\TestCase;
+
+class ClassessTest extends TestCase
+{
+    public function setUp()
+    {
+        //
+    }    
+}
