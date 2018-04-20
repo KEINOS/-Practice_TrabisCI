00f2b4580ada4272fe83d94d01034249e9d38ece
KEINOS <github@keinos.com>
2018-04-20 13:18:01 +0900

    chore: メソッド名の変更と注釈追加
    PHP5系とPHP7系の PHPUnit ではテスト用のクラス名が異なるが、PHPUnit 5の上位互換の記述で同一テスト・コードでテストするため。

--- tests/FunctionsTest.php
+++ tests/FunctionsTest.php
@@ -4,6 +4,12 @@ include('src/Functions.php');
 
 use PHPUnit\Framework\TestCase;
 
+//  PHP >= ^7.0 は PHPUnit ^6.0 であるため PHP >= ^5.6 の対応バージョン
+//  である PHPUnit 5.7.x の上位互換用の親クラスを継承
+//   ref: https://qiita.com/tanakahisateru/items/ef9f4c9b8ca39e6d0ed8
+//   PHPUnit_Framework_TestCase -> TestCase
+//class FunctionsTest extends PHPUnit_Framework_TestCase
+
 class FunctionsTest extends TestCase
 {
     public function setUp()
@@ -11,7 +17,7 @@ class FunctionsTest extends TestCase
         //
     }
     
-    public function testHelloWorld()
+    public function testEchoMessage()
     {
         $message = 'Hello, World!';
         $result  = echo_message($message);
